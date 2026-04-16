5b66f5e39d9937d9dd2631250fa81e8dc37e5306
Nabi-Ameen <nabiameen35126@gmail.com>
2026-04-16 15:51:52 +0500

    start ecommerec project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='h-[100px] bg-gray-200 flex items-center justify-center'>Footer</div>
+    )
+}
+
+export default Footer
---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typography from '../common/Typography'
+
+const Slider = ({ children }) => {
+    return (
+        <div>
+            {children}
+            {/* <Typography varient="small">Gaming Pc Complete Setup</Typography> */}
+        </div>
+    )
+}
+
+export default Slider
